97465485c8d26eee5b771142bd78baa7aa132465
Samuel Mendes <samuelmendespy@gmail.com>
2025-07-01 12:18:54 -0300

    feat: add constructors

--- backend/src/main/java/com/example/backend/domain/model/Pessoa.java
+++ backend/src/main/java/com/example/backend/domain/model/Pessoa.java
@@ -1,14 +1,18 @@
 package com.example.backend.domain.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Pessoa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
